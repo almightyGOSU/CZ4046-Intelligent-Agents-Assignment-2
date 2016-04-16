97a60778b2b2cf23f138e0195359609bcb277527
almightyGOSU <gosujinyao@gmail.com>
2016-04-16 22:15:45 +0800

    Updated GosuTheMinion

--- src/ThreePrisonersDilemma.java
+++ src/ThreePrisonersDilemma.java
@@ -1,18 +1,28 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ThreePrisonersDilemma {
 	
-	/* 
-	 This Java program models the two-player Prisoner's Dilemma game.
-	 We use the integer "0" to represent cooperation, and "1" to represent 
-	 defection. 
-	 
-	 Recall that in the 2-players dilemma, U(DC) > U(CC) > U(DD) > U(CD), where
-	 we give the payoff for the first player in the list. We want the three-player game 
-	 to resemble the 2-player game whenever one player's response is fixed, and we
-	 also want symmetry, so U(CCD) = U(CDC) etc. This gives the unique ordering
-	 
-	 U(DCC) > U(CCC) > U(DDC) > U(CDC) > U(DDD) > U(CDD)
-	 
-	 The payoffs for player 1 are given by the following matrix: */
+	/*
+	 * This Java program models the two-player Prisoner's Dilemma game. We use
+	 * the integer "0" to represent cooperation, and "1" to represent defection.
+	 * 
+	 * Recall that in the 2-players dilemma, U(DC) > U(CC) > U(DD) > U(CD),
+	 * where we give the payoff for the first player in the list. We want the
+	 * three-player game to resemble the 2-player game whenever one player's
+	 * response is fixed, and we also want symmetry, so U(CCD) = U(CDC) etc.
+	 * This gives the unique ordering
+	 * 
+	 * U(DCC) > U(CCC) > U(DDC) > U(CDC) > U(DDD) > U(CDD)
+	 * 
+	 * The payoffs for player 1 are given by the following matrix:
+	 */
 	
 	static int[][][] payoff = {  
 		{{6,3},  	// payoffs when first and second players cooperate 
@@ -20,16 +30,36 @@ public class ThreePrisonersDilemma {
 		{{8,5},  	// payoffs when first player defects, second coops
 	     {5,2}}};	// payoffs when first and second players defect
 	
-	/* 
-	 So payoff[i][j][k] represents the payoff to player 1 when the first
-	 player's action is i, the second player's action is j, and the
-	 third player's action is k.
-	 
-	 In this simulation, triples of players will play each other repeatedly in a
-	 'match'. A match consists of about 100 rounds, and your score from that match
-	 is the average of the payoffs from each round of that match. For each round, your
-	 strategy is given a list of the previous plays (so you can remember what your 
-	 opponent did) and must compute the next action.  */
+	private static void showPayoff() {
+
+		for (int i = 0; i < payoff.length; i++) {
+			for (int j = 0; j < payoff[0].length; j++) {
+				for (int k = 0; k < payoff[0][0].length; k++) {
+
+					System.out.printf(
+							"Player 1 %-10s Player 2 %-10s Player 3 %-10s --> "
+							+ " Player 1's payoff: %d%n",
+							i == 0 ? "Cooperate," : "Defect,",
+							j == 0 ? "Cooperate," : "Defect,",
+							k == 0 ? "Cooperate" : "Defect",
+							payoff[i][j][k]);
+				}
+			}
+		}
+		System.out.println();
+	}
+	
+	/*
+	 * So payoff[i][j][k] represents the payoff to player 1 when the first
+	 * player's action is i, the second player's action is j, and the third
+	 * player's action is k.
+	 * 
+	 * In this simulation, triples of players will play each other repeatedly in
+	 * a 'match'. A match consists of about 100 rounds, and your score from that
+	 * match is the average of the payoffs from each round of that match. For
+	 * each round, your strategy is given a list of the previous plays (so you
+	 * can remember what your opponent did) and must compute the next action.
+	 */
 	
 	
 	abstract class Player {
@@ -140,6 +170,7 @@ public class ThreePrisonersDilemma {
 			
 			if (n == 0)
 				return 0; // cooperate by default
+			
 			if (Math.random() < 0.5)
 				return oppHistory1[n - 1];
 			else
@@ -158,8 +189,60 @@ public class ThreePrisonersDilemma {
 	/* Gosu the Minion -- Note: Gosu the Minion is a NicePlayer */
 	class GosuTheMinion extends NicePlayer {
 		
+		// For tracking Def/Coop probabilities
+		private double opp1Def = 0;
+		private double opp2Def = 0;
+		
+		// Thresholds
+		private static final double BULLY_THRESHOLD = 0.75;
+		private static final double DEFENSIVE_THRESHOLD = 0.85;
+		
 		/* ALL HAIL KING CHODY!! */
+		int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
+			
+			// Start by picking a random action!
+			if(n == 0) {
+				return (int) Math.rint(1 * Math.random());
+			}
+			
+			// Calculate probability for Def/Coop (Opponent 1)
+			opp1Def += oppHistory1[n - 1];
+			double opp1DefProb = opp1Def / oppHistory1.length;
+			double opp1CoopProb = 1.000 - opp1DefProb;
+			
+			// Calculate probability for Def/Coop (Opponent 2)
+			opp2Def += oppHistory2[n - 1];
+			double opp2DefProb = opp2Def / oppHistory2.length;
+			double opp2CoopProb = 1.000 - opp2DefProb;
+			
+			/*System.out.printf("Opponent 1: %.3f, %.3f, Opponent 2: %.3f, %.3f%n",
+					opp1CoopProb, opp1DefProb, opp2CoopProb, opp2DefProb);*/			
+			
+			if (opp1CoopProb > BULLY_THRESHOLD
+					&& opp2CoopProb > BULLY_THRESHOLD
+					&& oppHistory1[n - 1] == oppHistory2[n - 1]
+					&& oppHistory1[n - 1] == 0) {
 
+				// Good chance both opponents will coop, def to maximize gain!
+				return 1;
+			}
+			else if (opp1DefProb > DEFENSIVE_THRESHOLD
+					&& opp2DefProb > DEFENSIVE_THRESHOLD
+					&& oppHistory1[n - 1] == oppHistory2[n - 1]
+					&& oppHistory1[n - 1] == 1) {
+
+				// Good chance both opponents will def, def to protect myself!
+				return 1;
+			}
+			else {
+				// Follow previous action of the 'friendlier' opponent
+				if (opp1CoopProb > opp2CoopProb) {
+					return oppHistory1[n - 1];
+				} else {
+					return oppHistory2[n - 1];
+				}
+			}
+		}
 	}
 	/* Gosu the Minion */
 
@@ -205,9 +288,11 @@ public class ThreePrisonersDilemma {
 		return result;
 	}
 	
-	/* The procedure makePlayer is used to reset each of the Players 
-	 (strategies) in between matches. When you add your own strategy,
-	 you will need to add a new entry to makePlayer, and change numPlayers.*/
+	/*
+	 * The procedure makePlayer is used to reset each of the Players
+	 * (strategies) in between matches. When you add your own strategy, you will
+	 * need to add a new entry to makePlayer, and change numPlayers.
+	 */
 	
 	// TODO: Change numPlayers
 	int numPlayers = 8;
@@ -221,7 +306,7 @@ public class ThreePrisonersDilemma {
 		case 4: return new FreakyPlayer();
 		case 5: return new T4TPlayer();
 		
-		//TODO: Add entry to makePlayer
+		// TODO: Add entry to makePlayer
 		case 6: return new KingChody();
 		case 7: return new GosuTheMinion();
 		}
@@ -230,13 +315,84 @@ public class ThreePrisonersDilemma {
 	
 	/* Finally, the remaining code actually runs the tournament. */
 	
+	private static Map<String, Integer> map1stPlace = new HashMap<>();
+	private static Map<String, Integer> mapTop3 = new HashMap<>();
+	
 	public static void main (String[] args) {
 		
 		ThreePrisonersDilemma instance = new ThreePrisonersDilemma();
-		instance.runTournament();
+		showPayoff();
+		
+		for (int i = 0; i < instance.numPlayers; i++) {
+			String playerName = instance.makePlayer(i).name();
+			map1stPlace.put(playerName, 0);
+			mapTop3.put(playerName, 0);
+		}
+		
+		// TODO: Change number of tournaments to be executed!
+		int totalTour = 5000;
+		for(int numTour = 0; numTour < totalTour; numTour++) {
+			instance.runTournament();
+			System.out.println();
+		}
+		
+		playerScores = sortByDoubleValuesDesc(playerScores);
+		System.out.println("Average score after " + totalTour + " tournaments:");
+		for(Map.Entry<String, Double> playerScore : playerScores.entrySet()) {
+			
+			System.out.printf("%-20s %.3f points.%n",
+					playerScore.getKey(),
+					playerScore.getValue() / totalTour);
+		}
+		
+		System.out.println("\n==================================="
+				+ " Rankings! ===================================");
+		System.out.printf("%-25s %-30s %-20s%n",
+				"Player Name", "1st Place", "Top 3");
+		for(Map.Entry<String, Integer> mEntry : map1stPlace.entrySet()) {
+			
+			System.out.printf("%-25s ", mEntry.getKey());
+			int numTop1 = mEntry.getValue();
+			int numTop3 = mapTop3.get(mEntry.getKey());
+			System.out.printf("%-30s ", String.format("%-12s (%2.2f%%) ",
+					(numTop1 + "/" + totalTour), ((double) numTop1 / totalTour) * 100));
+			System.out.printf("%-12s (%2.2f%%)%n",
+					(numTop3 + "/" + totalTour), ((double) numTop3 / totalTour) * 100);
+		}
+		System.out.println("==================================="
+				+ " Rankings! ===================================");
 	}
 	
-	boolean verbose = true; // set verbose = false if you get too much text output
+	private static Map<String, Double> playerScores = new HashMap<>();
+	
+	/**
+     * Used for sorting a Map by its Double values, in descending order<p>
+     * Returned map is a LinkedHashMap which preserves order
+     **/
+    public static Map<String, Double> sortByDoubleValuesDesc(Map<String, Double> map) {
+
+        if (map == null)
+            return map;
+
+        List<Map.Entry<String, Double>> list = new LinkedList<>(map.entrySet());
+
+        // Sort based on values, in descending order
+        Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+            public int compare(Map.Entry<String, Double> mapEntry1, Map.Entry<String, Double> mapEntry2) {
+                return mapEntry2.getValue().compareTo(mapEntry1.getValue());
+            }
+        });
+
+        // Using LinkedHashMap to preserve order
+        Map<String, Double> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> mapEntry : list) {
+            sortedMap.put(mapEntry.getKey(), mapEntry.getValue());
+        }
+
+        return sortedMap;
+    }
+	
+	boolean verbose = false; // set verbose = false if you get too much text output
 	
 	void runTournament() {
 
@@ -289,11 +445,33 @@ public class ThreePrisonersDilemma {
 		// Finally, print out the sorted results.
 		if (verbose)
 			System.out.println();
+		
 		System.out.println("Tournament Results");
 		for (int i = 0; i < numPlayers; i++) {
+			
+			String playerName = makePlayer(sortedOrder[i]).name();
 			System.out.printf("%-20s %.3f points.%n",
-					makePlayer(sortedOrder[i]).name() + ":",
-					totalScore[sortedOrder[i]]);
+					playerName, totalScore[sortedOrder[i]]);
+			
+			// Average score in this tournament
+			Double currentScore = playerScores.get(playerName);
+			if(currentScore != null) {
+				currentScore += totalScore[sortedOrder[i]];
+			}
+			else {
+				currentScore = Double.valueOf(totalScore[sortedOrder[i]]);
+			}
+			playerScores.put(playerName, currentScore);
+			
+			if (i < 1) {
+				Integer numTop1 = map1stPlace.get(playerName);
+				map1stPlace.put(playerName, (numTop1 == null) ? 1 : numTop1 + 1);
+			}
+			
+			if (i < 3) {
+				Integer numTop3 = mapTop3.get(playerName);
+				mapTop3.put(playerName, (numTop3 == null) ? 1 : numTop3 + 1);
+			}
 		}
 
 	} // end of runTournament()
